4670d370383da04684162bac23259f6c458133f5
Sreedhara GS <sreedhara.gs@asahi-gh.com>
2026-01-26 19:59:31 +0900

    Remove Vasant Panchami and past events

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { UpdatesProvider } from "@/context/UpdatesContext";
 import HomeworkDuePopup from '@/components/HomeworkDuePopup';
-import VasantPanchamiPopup from '@/components/VasantPanchamiPopup';
+
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import InstallPrompt from "@/components/InstallPrompt";
@@ -53,7 +53,7 @@ export default function RootLayout({
         <UpdatesProvider>
           <Navigation />
           <HomeworkDuePopup />
-          <VasantPanchamiPopup />
+
           <main className="min-h-screen pb-20">
             {children}
           </main>
